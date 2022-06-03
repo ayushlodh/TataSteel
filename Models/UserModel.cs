ad5f77bc1302802945055b44f6326e5eb6bcd3ed
ayushlodh <87244116+ayushlodh@users.noreply.github.com>
2022-06-03 19:37:48 +0530

    Add files via upload

---
+++ Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TataSteel.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int Contact { get; set; }
+        public string Department { get; set; }
+        public string Role { get; set; }
+
+    }
+}
